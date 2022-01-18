2ccc9d0801686dba958184f5226494ddd680b231
y0s0k0 <y0s0k0.0521@gmail.com>
2022-01-19 05:38:53 +0900

    deploy setting
    デプロイの設定修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1' 
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/app-portfolio.pem'] 
+                  keys: ['~/.ssh/portfolio (1).pem'] 
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
